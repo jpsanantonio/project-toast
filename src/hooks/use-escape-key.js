f21ec030aad0504244a2259db245b28240fdc3ed
Jayvic San Antonio <codes@jayvicsanantonio.dev>
2024-11-08 17:25:09 -0800

    Address review comments

--- src/hooks/use-escape-key.js
+++ src/hooks/use-escape-key.js
@@ -1,6 +1,10 @@
 import { useEffect } from "react";
 
 export default function useEscapeKey(callback) {
+  if (typeof callback !== "function") {
+    throw new Error("useEscapeKey must be used with a function");
+  }
+
   useEffect(() => {
     const handleKeyDown = (event) => {
       if (event.key !== "Escape") return;
